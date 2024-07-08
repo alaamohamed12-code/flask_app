2fecb81ebf070f1ac31f0b44e9fe5861fbd3540d
alaamohamed12-code <68365922+alaamohamed12-code@users.noreply.github.com>
2024-07-08 04:21:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -260,4 +260,5 @@ def fetch():
     return render_template('index2.html', video_info=video_info)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
